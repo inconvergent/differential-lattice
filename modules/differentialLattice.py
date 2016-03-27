d36477b784dbb54636f747dcf81f591aab1590d1
Anders Hoff <inconvergent@gmail.com>
2016-03-27 16:23:10 +0200

    reset every n

--- modules/differentialLattice.py
+++ modules/differentialLattice.py
@@ -10,16 +10,21 @@ from scipy.spatial import cKDTree as kdt
 from numpy import pi
 from numpy import any
 from numpy import logical_and
-from numpy import sum
+from numpy import logical_or
+from numpy import max
 from numpy import mean
 from numpy.linalg import norm
 from numpy import arange
 from numpy import zeros
+from numpy import column_stack
+from numpy import sin
+from numpy import cos
 from numpy import ones
 from numpy import reshape
 from numpy.random import random
 
-from dddUtils.random import darts
+from scipy.spatial.distance import cdist
+
 
 
 TWOPI = pi*2
@@ -49,27 +54,27 @@ class DifferentialLattice(object):
     self.size = size
     self.one = 1.0/size
 
-    self.stp = 5e-4
+    self.stp = 4e-4
     self.spring_stp = 1
     self.reject_stp = 0.5
 
     self.nmax = nmax
     self.max_capacity = 5
-    self.min_capacity = 3
+    self.min_capacity = 2
 
-    self.capacity_cool_down = 50
+    self.capacity_cool_down = 20
 
     self.node_rad = 5*self.one
     self.disconnect_rad = 2*self.node_rad
     self.inner_influence_rad = 2*self.node_rad
-    self.outer_influence_rad = 4*self.node_rad
+    self.outer_influence_rad = 10*self.node_rad
 
     self.fn = Fn(prefix=prefix, postfix='.png')
     self.render = Animate(size, back, front, self.step)
     self.render.set_line_width(self.one)
 
     self.__init()
-    self.spawn(5000)
+    self.spawn(2000, dst=self.node_rad*0.5)
 
     self.render.start()
 
@@ -85,11 +90,14 @@ class DifferentialLattice(object):
     self.capacities = zeros((nmax, 1), 'int') + self.max_capacity
     self.num_edges = zeros((nmax, 1), 'int')
 
-  def spawn(self, n):
+  def spawn(self, n, dst):
+
+    # from dddUtils.random import darts
 
     num = self.num
-
-    new_xy = darts(n, 0.5, 0.5, 0.4, self.node_rad*1.5)
+    # new_xy = darts(n, 0.5, 0.5, 0.4, dst)
+    theta = random(n)*TWOPI
+    new_xy = 0.5 + column_stack([cos(theta), sin(theta)])*0.4
     new_num = len(new_xy)
     if new_num>0:
       self.xy[num:num+new_num,:] = new_xy
@@ -131,24 +139,48 @@ class DifferentialLattice(object):
     self.num_edges[a] -= 1
     self.num_edges[b] -= 1
 
+  def __is_relative_neighbor(self, i, cands):
+
+    if len(cands)<2:
+      return ones(1, 'bool')
+
+    xy = self.xy
+
+    inds = cands + [i]
+    dists = cdist(xy[inds,:], xy[inds,:])
+
+    uv = dists[:-1,:-1]
+    us = dists[:-1,-1]
+    mas = max(uv, axis=0)
+    return us<mas
+
+  def reset_structure(self):
+
+    num = self.num
+    self.num_edges[:num,0] = 0
+
   def structure(self):
 
     num = self.num
-
     potentials = self.num_edges[:num,0] < self.capacities[:num,0]
     potentials_inds = arange(num)[potentials]
     candidate_sets = self.tree.query_ball_point(self.xy[potentials_inds,:], self.disconnect_rad)
 
     for i, cands in zip(potentials_inds, candidate_sets):
 
-      for c in cands:
-        if i == c:
-          continue
+      cands = [c for c in cands if c != i]
+      rel = self.__is_relative_neighbor(i, cands)
+
+      for j,c in enumerate(cands):
         if self.num_edges[i]>=self.max_capacity:
           break
         if self.num_edges[c]>=self.max_capacity:
           continue
-        if potentials[c] and not self.is_connected(i, c):
+        if not potentials[c]:
+          continue
+        if self.is_connected(i, c):
+          continue
+        if rel[j]:
           self.connect(i, c)
 
     # reduced = self.capacities[:num,0] < self.max_capacity
@@ -186,21 +218,25 @@ class DifferentialLattice(object):
         dx = xy[e,:]-xy[i,:]
         dd = norm(dx, axis=1)
         reject = dd<self.node_rad*1.8
-        mid = logical_and(
-          dd>self.node_rad*1.8,
-          dd<self.node_rad*2.2
+        mid = logical_or(
+          dd<self.node_rad*1.8,
+          dd>self.node_rad*2.2
         )
-        dx /= reshape(dd,(-1,1))
-        dx[reject] *= -1
-        dx[mid] = 0
-        dxy[i,:] += reshape(mean(dx, axis=0), 2)*self.spring_stp
+        if any(mid):
+          dx /= reshape(dd,(-1,1))
+          dx[reject] *= -1
+          dxy[i,:] += reshape(mean(dx[mid,:], axis=0), 2)*self.spring_stp
 
+      # unconnected
       out = set([i]+list(e))
       cands = [c for c in candidate_sets[i] if c not in out]
       if len(cands)>1:
-        inv = 1
+        inv = ones(len(cands), 'float')
+
         if potentials[i]:
-          inv = -1
+          p = potentials[cands,0]
+          inv[p] *= -1
+
         dx = xy[i,:]-xy[cands,:]
         dd = norm(dx, axis=1)
         force = (self.outer_influence_rad-dd)/self.outer_influence_rad
@@ -214,14 +250,14 @@ class DifferentialLattice(object):
     self.itt += 1
     print(self.itt)
 
+    self.reset_structure()
+    self.make_tree()
+    self.structure()
     for i in xrange(self.repeats):
-      self.make_tree()
-      self.structure()
-      self.spawn(5)
       self.forces()
 
     self.show()
-    # self.render.write_to_png(self.fn.name())
+    self.render.write_to_png(self.fn.name())
 
     return True
 
@@ -242,11 +278,11 @@ class DifferentialLattice(object):
     for i in xrange(self.num):
       if potentials_flag[i]:
         self.render.ctx.set_source_rgba(*CYAN)
-        arc(xy[i,0], xy[i,1], node_rad, 0, TWOPI)
+        arc(xy[i,0], xy[i,1], 0.5*node_rad, 0, TWOPI)
         fill()
         self.render.ctx.set_source_rgba(*FRONT)
 
-      arc(xy[i,0], xy[i,1], node_rad, 0, TWOPI)
+      arc(xy[i,0], xy[i,1], 0.5*node_rad, 0, TWOPI)
       stroke()
 
     self.render.ctx.set_source_rgba(*FRONT)
